7c8e30578621247d3e1c1352641dd1a9432d35bf
AWSafran <andrewsafran@gmail.com>
2019-04-25 15:56:34 -0400

    added rugby classes

--- athlete/src/com/lambdaschool/solution/ProcessInjector.java
+++
@@ -1,7 +0,0 @@
-package com.lambdaschool.solution;
-
-public interface ProcessInjector
-{
-    //All injectors must have a getProcess() method that returns a processor
-    public Processor getProcess();
-}
---
+++ athlete/src/com/lambdaschool/solution/RugbyAthleteProcessor.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.solution;
+
+public class RugbyAthleteProcessor implements Processor {
+    @Override
+    public void displayAthlete() {
+        System.out.println("************" + "\nRugby Athlete\n" + "************");
+    }
+}
